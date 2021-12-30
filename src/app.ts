ad9640e8abf4a708069bf4933644727daf24cdef
amitshky <a7shky@gmail.com>
2021-12-30 13:51:40 +0545

    db connection; healthcheck route

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,22 @@
-import speakeasy from 'speakeasy'
-import express   from 'express'
-import qrcode    from 'qrcode'
+import config from 'config'
+import express from 'express'
+
+import logger from './common/logger'
+import connect from './common/db/connect'
+import routes from './server/routes'
 
 
-const secretKey: string =  'OM4EU4DLNUVGCTROG4QUST3MM52SCRLPGJYHCPRQN45UCYJOKAQQ';  // TODO: store this in an env variable
+const HOST = config.get('host') as string;
+const PORT = config.get('port') as number;
+const app  = express();
 
-const app: express.Application = express();
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static('./public/'));            // static assets
-app.use(express.urlencoded({ extended: true })); // parse data
-app.use(express.json());                         // parse json
-
-app.get('/', (req: express.Request, res: express.Response) => res.status(200).sendFile('index.html'));
-
-app.get('/totp/generate', (req: express.Request, res: express.Response) =>
+app.listen(PORT, HOST, (): void => 
 {
-	const secret: speakeasy.GeneratedSecret = speakeasy.generateSecret( { name: "Skeeob" });
-	qrcode.toDataURL(secret.otpauth_url!, (err: Error, data: string) => 
-	{
-		if (err)
-		{
-			console.log(err);
-			return res.status(500).send('Unable to generate QR code!');
-		}
-		else
-		{
-			const qrcodeData: string = "<img src=\"" + data + "\">\n" +
-				"<code>" +
-				"\n<br>Secret key (base32): " + secret.base32 + 
-				//"\n<br>hex: " + secret.hex +
-				"\n</code>";
-			return res.status(200).send(qrcodeData);
-		}
-	});
+	logger.info(`Listening on http://${HOST}:${PORT}`);
+	connect();
+	routes(app);
 });
-
-app.post('/totp/validate', (req: express.Request, res: express.Response) =>
-{
-	const { token } = req.body;
-	if (!token)
-		return res.status(401).json({ success: false, message: 'Please provide a token'});
-
-	const verified: boolean = speakeasy.totp.verify({
-		secret: secretKey,
-		encoding: 'base32',
-		token: token
-	});
-
-	if (!verified)
-		return res.status(401).json({ success: false, message: 'Unauthorized'});
-	
-	return res.status(200).json({ success: true, message: 'Token verified'});
-});
-
-app.listen(5000, () => console.log('Listening on: 5000...'));
