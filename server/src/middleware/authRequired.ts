db727eab26e95716762060e0159d46479d007901
NKS <eminem.nikesh@gmail.com>
2022-02-27 16:23:58 +0545

    authRequired middleware added with placeholders

--- server/src/middleware/authRequired.ts
+++ server/src/middleware/authRequired.ts
@@ -1,1 +1,12 @@
 import {Request,Response, NextFunction} from 'express';
+import {get} from 'lodash';
+
+const authRequired = async (req: Request, res: Response, next: NextFunction) =>{
+    const user = get(req, 'user');
+    if(!user)
+        return res.sendStatus(403);
+    
+    return next();   
+}
+
+export default authRequired;
