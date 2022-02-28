675579fd4a4633679bb633ae31669aff44ab26a7
NKS <eminem.nikesh@gmail.com>
2022-02-28 18:32:09 +0545

    response for non user changed

--- server/src/middleware/requiresUser.ts
+++ server/src/middleware/requiresUser.ts
@@ -1,12 +1,13 @@
 import { get } from 'lodash'
 import { Request, Response, NextFunction } from 'express'
+import path from 'path'
 
-
+const publicPath = path.resolve(__dirname, '../public/');
 const requiresUser = async (req: Request, res: Response, next: NextFunction) =>
 {
 	const user = get(req, 'user');
 	if (!user)
-		return res.sendStatus(403); // access to the requested resource is forbidden
+		return res.sendFile(publicPath + '/authRequired.html'); // access to the requested resource is forbidden
 	
 	return next();
 }
